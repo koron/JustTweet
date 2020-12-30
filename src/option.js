f16987c2ca6704280d22b67c7985bf64efc8c13a
yoshiteru <yoshiteru-nakamura@users.noreply.github.com>
2020-12-31 07:19:16 +0900

    make branch

--- src/option.js
+++ src/option.js
@@ -1,10 +1,11 @@
 ﻿window.onload = function(){
-     var prefix = localStorage['Prefix'];
+    var prefix = localStorage.getItem('Prefix');
     if (prefix == null) {
         prefix = 'NowBrowsing: ';
     }
     document.getElementById('id_prefix').value = prefix;
 }
 document.getElementById('id_save').onclick = function() {
-    localStorage['Prefix'] = document.getElementById('id_prefix').value;
+    localStorage.setItem('Prefix' , document.getElementById('id_prefix').value);
+    localStorage.setItem('position' , document.getElementById('window_position').value);
 }
