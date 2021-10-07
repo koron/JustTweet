053d35f1dd9c1fc62f0e836f84a3513ccc682743
yoshiteru <yoshiteru-nakamura@users.noreply.github.com>
2021-10-07 23:40:54 +0900

    v3 test

--- src_V3test/option.js
+++ src_V3test/option.js
@@ -1,10 +1,17 @@
 ﻿window.onload = function(){
-    let prefix = localStorage.getItem('Prefix');
+    // let prefix = localStorage.getItem('Prefix');
+    let prefix = chrome.storage.local.get("prefix", function (value) {
+        var value_data = value.key;
+      });
+
     if (prefix == null) {
         prefix = 'NowBrowsing: ';
     }
     document.getElementById('id_prefix').value = prefix;
 
+
+
+    
     let position = localStorage.getItem('position');
     if (position == null) {
         position = '1';
@@ -12,7 +19,10 @@
 }
 
 document.getElementById('id_save').onclick = function() {
-    localStorage.setItem('Prefix' , document.getElementById('id_prefix').value);
+    // localStorage.setItem('Prefix' , document.getElementById('id_prefix').value);
+     chrome.storage.local.set({'prefix': document.getElementById('id_prefix').value}, function () {
+    });
+
     let positions = document.getElementsByName("name_position");
     for(let i = 0; i < positions.length; i++){
         if(positions[i].checked) {
